8cccd6cb322e553e9f8c09b3a760d94fc2edf44c
Ashish Sharda <ashishjsharda@gmail.com>
2023-06-15 05:15:30 -0500

    update pom and main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.jobrunr.configuration.JobRunr;
+import org.jobrunr.scheduling.BackgroundJob;
+import org.jobrunr.storage.InMemoryStorageProvider;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JobRunr.configure().useStorageProvider
+                (new InMemoryStorageProvider()).useBackgroundJobServer().useDashboard().initialize();
+        BackgroundJob.enqueue(() -> System.out.println("Hello World!"));
     }
 }
